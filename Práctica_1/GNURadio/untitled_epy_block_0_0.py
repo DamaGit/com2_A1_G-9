61172611a132e5e20e088888b87101cbd29900fb
DamaGit <90076437+DamaGit@users.noreply.github.com>
2025-08-26 21:11:15 -0500

    Update untitled_epy_block_0_0.py

--- Práctica_1/GNURadio/untitled_epy_block_0_0.py
+++ Práctica_1/GNURadio/untitled_epy_block_0_0.py
@@ -1,22 +1,25 @@
 import numpy as np
 from gnuradio import gr
 
-class blk (gr.sync_block):
-    def __init__( self ) : # only default arguments here
+class blk(gr.sync_block):  
+    def __init__(self):  
         gr.sync_block.__init__(
             self,
-            name = "e_Dif", # will show up in GRC
-            in_sig =[np.float32],
-            out_sig =[np.float32]
-        )    
+            name='e_Dif',   
+            in_sig=[np.float32],
+            out_sig=[np.float32]
+        )
         self.acum_anterior = 0
-    
-
-    def work (self,input_items,output_items):
-        x = input_items [0] # Senial de entrada .
-        y0 = output_items [0] # Senial acumulada
+        
+    def work(self, input_items, output_items):
+        x = input_items[0]  # Señal de entrada
+        y = output_items[0]  # Señal de salida
         N = len(x)
-        diff = np.cumsum(x) - self.acum_anterior
-        self.acum_anterior = diff[N-1]
-        y0 [:] = diff
-        return len (y0)
+
+        # Diferenciación acumulada: y[n] = x[n] - acum_anterior
+        for i in range(N):
+            y[i] = x[i] - self.acum_anterior  # Restamos el valor acumulado anterior
+            self.acum_anterior = x[i]  # Actualizamos el acumulado con el valor actual
+        
+        return len(y)
+    
